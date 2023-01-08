7c46717b06a558453d2cfe865b3210ce6912449a
nikhilsomani15 <nikhilsomani15@gmail.com>
2023-01-08 22:33:33 +0530

    JavaSript file modified

--- JavaScript/objects.js
+++ JavaScript/objects.js
@@ -1,74 +1,72 @@
 const object = {
-    rocket: '🚀',
-    home: '🏡'
-}
+  rocket: "🚀",
+  home: "🏡",
+};
 
 // string, number, bigint, boolean, null, undefine, symbol
 
 const obj1 = {
-    rocket: '🚀',
-    fuel: 2,
-    config: {
-        name: 'mars'
-    }
-}
+  rocket: "🚀",
+  fuel: 2,
+  config: {
+    name: "mars",
+  },
+};
 
-obj1.fuel = 200
-obj1['year'] = 2000
-
+obj1.fuel = 200;
+obj1["year"] = 2000;
+console.log(obj1);
 // part 2
 
-const obj2 = new Object() //constructor
-obj2.redbook = '📕'
+const obj2 = new Object(); //constructor
+obj2.redbook = "📕";
 // obj2.1bluebook = '📘'
-obj2.myValue = '1 blue book'
+obj2.myValue = "1 blue book";
 
-// console.log(obj2);
+console.log(obj2);
 
 // part 3
 const powers = {
-    fly: true,
-    cordinate: Math.random() + 2
-}
+  fly: true,
+  cordinate: Math.random() + 2,
+};
 
-const obj3 = Object.create(powers)
-// console.log(obj3);
-// console.log(obj3.cordinate);
-// console.log(Object.getPrototypeOf(obj3));
+const obj3 = Object.create(powers);
+console.log(obj3);
+console.log(obj3.cordinate);
+console.log(Object.getPrototypeOf(obj3));
 
 //part 4
 
-const obj4 = Object.create({})
-Object.defineProperty(obj4, 'book', {
-    // icon: '📘'
-    get: () => '📘',
-    enumerable: true
-})
+const obj4 = Object.create({});
+Object.defineProperty(obj4, "book", {
+  // icon: '📘'
+  get: () => "📘",
+  enumerable: true,
+});
 
-// console.log(obj4);
-// console.log(obj4.book);
-// console.log(Object.getPrototypeOf(obj4));
+console.log(obj4);
+console.log(obj4.book);
+console.log(Object.getPrototypeOf(obj4));
 
-for(k in obj4){
-    //console.log("value is: ", k);
+for (k in obj4) {
+  console.log("value is: ", k);
 }
 
 //part 5
 const obj5 = {
-    comics : 'marvel',
-    pen: '',
-    printComic: function(){
-        this.pen += '🖊️🗒️'
-        console.log(this);
-        return this;
-    },
-    // printaComic: () => {
-    //     // this.pen += '🖊️🗒️'
-    //     console.log(this);
+  comics: "marvel",
+  pen: "",
+  printComic: function () {
+    this.pen += "🖊️🗒️";
+    console.log(this);
+    return this;
+  },
+  printaComic: () => {
+    this.pen += "🖊️🗒️";
+    console.log(this);
+  },
+};
 
-    // },
-    
-}
-
-console.log(obj5.printComic().printComic().printComic());
-// console.log(obj5.printaComic());
+// console.log(obj5.printComic().printComic().printComic());
+console.log(obj5.printComic());
