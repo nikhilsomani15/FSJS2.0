3f41738bd3975077be7827ec8b75294ed3c00c3e
nikhilsomani15 <nikhilsomani15@gmail.com>
2023-02-10 19:17:53 +0530

    JavaScript Assignment 10 solution added

---
+++ JavaScript/JavaScript Assignment/JavaScript Assignment 10/script.js
@@ -0,0 +1,14 @@
+console.log(4 > 3); //true
+console.log(4 >= 3); //true
+console.log(4 < 3); //false
+console.log(4 <= 3); //false
+console.log(4 == 4); //true
+console.log(4 === 4); //true
+console.log(4 != 4); //false
+console.log(4 !== 4); //false
+console.log(4 != "4"); // false
+console.log(4 == "4"); //true
+console.log(4 === "4"); //false
+let p = "python".length;
+let j = "jargon".length;
+console.log(p !== j);
