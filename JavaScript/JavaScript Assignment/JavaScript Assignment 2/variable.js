110b68d2ab22d4e7435917f0c75087f4a2a66546
nikhilsomani15 <nikhilsomani15@gmail.com>
2023-02-10 18:05:28 +0530

    JavaScript Assignment 2 solution added

---
+++ JavaScript/JavaScript Assignment/JavaScript Assignment 2/variable.js
@@ -0,0 +1,8 @@
+let fName = "Nikhil";
+let lName = "Somani";
+let maritalStatus = "single";
+let country = "India";
+let age = 22;
+console.log(
+  `The first name is ${fName} lastname is ${lName} is ${maritalStatus} lives in ${country} and age is ${age}`
+);
