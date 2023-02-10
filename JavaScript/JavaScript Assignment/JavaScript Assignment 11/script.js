b5cd924ca235dd0c904eb5d4d40175903bd88d9a
nikhilsomani15 <nikhilsomani15@gmail.com>
2023-02-10 19:24:16 +0530

    JavaScript Assignment 11 solution added

---
+++ JavaScript/JavaScript Assignment/JavaScript Assignment 11/script.js
@@ -0,0 +1,8 @@
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth() + 1;
+let todayDate = date.getDate();
+let day = date.getDay();
+let hour = date.getHours();
+let min = date.getMinutes();
+console.log(year, month, todayDate, day, hour, min);
