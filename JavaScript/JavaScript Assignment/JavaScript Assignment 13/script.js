17c1b8ee023413cc17aa626f0583eef1b6aec71d
nikhilsomani15 <nikhilsomani15@gmail.com>
2023-02-11 01:53:43 +0530

    JavaScript Assignment 14 solution added

--- JavaScript/JavaScript Assignment/JavaScript Assignment 13/script.js
+++ JavaScript/JavaScript Assignment/JavaScript Assignment 13/script.js
@@ -2,5 +2,5 @@ let userInput = parseInt(prompt("Enter your age"));
 if (userInput >= 18) {
   alert("You can drive");
 } else {
-  alert(`You can drive after ${18 - userInput} years`);
+  alert(`You can drive after ${18 - userInput} Years`);
 }
