7a047ebe44d2cd261a0c72a85d059e6a4c6d6d70
nikhilsomani15 <nikhilsomani15@gmail.com>
2023-02-10 18:41:15 +0530

    JavaScript Assignment 7 solution added

---
+++ JavaScript/JavaScript Assignment/JavaScript Assignment 7/script.js
@@ -0,0 +1,4 @@
+let arr = ["Nikhil", "pw skills", "Somani", "pw skills"];
+let lastIndex = arr.lastIndexOf("pw skills");
+console.log("Last occurence is", arr.lastIndexOf("pw skills"));
+console.log("First occurence", arr.lastIndexOf("pw skills", lastIndex - 1));
