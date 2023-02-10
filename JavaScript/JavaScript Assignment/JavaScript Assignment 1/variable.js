405a906380955d24f2c2346dbf0bf3eef982cd20
nikhilsomani15 <nikhilsomani15@gmail.com>
2023-02-10 18:01:30 +0530

    JavaScript Assignment 1 solution added

---
+++ JavaScript/JavaScript Assignment/JavaScript Assignment 1/variable.js
@@ -0,0 +1,8 @@
+let str = "Nikhil";
+let bool = true;
+let undef = undefined;
+let nul = null;
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof undef);
+console.log(typeof nul);
