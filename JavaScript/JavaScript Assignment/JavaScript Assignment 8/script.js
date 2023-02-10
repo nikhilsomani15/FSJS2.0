6f15fe5bbafd2bf2d4d89f4de198330507e4da4d
nikhilsomani15 <nikhilsomani15@gmail.com>
2023-02-10 18:50:25 +0530

    JavaScript Assignment 8 solution added

---
+++ JavaScript/JavaScript Assignment/JavaScript Assignment 8/script.js
@@ -0,0 +1,5 @@
+let stringWithSpace = "    Nikhil     ";
+let strLenSpace = stringWithSpace.length;
+let strLenWithoutSpace = strLenSpace.trim().length;
+console.log(strLenSpace); //15
+console.log(strLenWithoutSpace); //6
