06f8ba5f8f16a416dff67ebadf73a35a0c4005fa
nikhilsomani15 <nikhilsomani15@gmail.com>
2023-02-10 18:59:45 +0530

    JavaScript Assignment 9 solution added

---
+++ JavaScript/JavaScript Assignment/JavaScript Assignment 9/script.js
@@ -0,0 +1,9 @@
+let true1 = 1;
+let true2 = "Nikhil";
+let true3 = true;
+let false1 = 0;
+let false2 = undefined;
+let false3 = null;
+
+console.log(Boolean(true1), Boolean(true2), Boolean(true3));
+console.log(Boolean(false1), Boolean(false2), Boolean(false3));
