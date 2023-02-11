ecd58ebd632517046c4315f04580a42124a5fe42
nikhilsomani15 <nikhilsomani15@gmail.com>
2023-02-12 01:21:26 +0530

    JavaScript Assignment 1 solution added

---
+++ JavaScript/JavaScript Assignment/JavaScript Assignment 17/script.js
@@ -0,0 +1,18 @@
+// Write a program which tells the number of days in a month.
+let months = {
+  January: 31,
+  February: 28,
+  March: 31,
+  April: 30,
+  May: 31,
+  June: 30,
+  July: 31,
+  August: 31,
+  September: 30,
+  October: 31,
+  November: 30,
+  December: 31,
+};
+for (const key in months) {
+  console.log(`${key} has total ${months[key]} days`);
+}
