7e7b4149b06f92fb0a453a2d8959e64f9efd603d
nikhilsomani15 <nikhilsomani15@gmail.com>
2023-02-12 01:53:31 +0530

    JavaScript Assignment 18 solution added

---
+++ JavaScript/JavaScript Assignment/JavaScript Assignment 18/script.js
@@ -0,0 +1,18 @@
+// Write a program which tells the number of days in a month, now consider leap year.
+let months = {
+  January: 31,
+  February: 29,
+  March: 31,
+  April: 30,
+  May: 31,
+  June: 30,
+  July: 31,
+  August: 31,
+  September: 30,
+  October: 31,
+  November: 30,
+  December: 31,
+};
+for (const key in months) {
+  console.log(`${key} has total of ${months[key]} days`);
+}
