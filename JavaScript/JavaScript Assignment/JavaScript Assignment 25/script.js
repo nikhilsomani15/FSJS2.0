b5b206cd5021c29c355a49a0cc3b9c55414fd160
nikhilsomani15 <nikhilsomani15@gmail.com>
2023-02-15 23:57:18 +0530

    JavaScript Assignment 25 solution updated

--- JavaScript/JavaScript Assignment/JavaScript Assignment 25/script.js
+++ JavaScript/JavaScript Assignment/JavaScript Assignment 25/script.js
@@ -15,7 +15,7 @@ if (bmi < 18.5) {
 } else if (bmi >= 18.5 && bmi <= 24.9) {
   console.log("Normal weight");
 } else if (bmi >= 25 && bmi <= 29.9) {
-  console.log("Normal weight");
+  console.log("Overweight");
 } else {
   console.log("Obese");
 }
