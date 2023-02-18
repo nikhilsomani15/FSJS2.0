51026450299d3133fffb8a9d9b6031202c435e70
nikhilsomani15 <nikhilsomani15@gmail.com>
2023-02-19 00:47:41 +0530

    React counter Project updatedd

--- ReactJS/Project/counter-app/src/App.js
+++ ReactJS/Project/counter-app/src/App.js
@@ -2,16 +2,18 @@ import React, { useState } from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 function App() {
-  const [count, setCount] = useState(10);
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <header>Counter App Using state</header>
       <h2>Current value of count is {count}</h2>
-      <button onClick={() => setCount(0)}>Reset</button>
+      <button onClick={() => setCount(10)}>Reset</button>
+      <br />
       <button onClick={() => setCount(count + 1)}>Increse</button>
-      <button onClick={() => (count > 0 ? setCount(count - 1) : "")}>
-        Decrese
+      <br />
+      <button onClick={() => (count <= 0 ? "" : setCount(count - 1))}>
+        Decrese {count}
       </button>
     </div>
   );
