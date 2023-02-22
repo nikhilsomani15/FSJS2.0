a8c95a8ec720b27f459762b06280a1d2662747ea
nikhilsomani15 <nikhilsomani15@gmail.com>
2023-02-23 00:00:25 +0530

    React 18 feb file updated

--- ReactJS/reacthc(18 Feb)/exampleReact.js
+++ ReactJS/reacthc(18 Feb)/exampleReact.js
@@ -1,16 +1,13 @@
-function setState(){
-    let score = 22
-    return [score]
+function setState() {
+  let score = 22;
+  return [score];
 }
 
-let [myScore] = setState()
+let [myScore] = setState();
 myScore;
-myScore = 12
+myScore = 12;
 console.log(myScore);
 
-let [anotherScore] = setState()
-anotherScore;
+let [anotherScore] = setState();
 
 console.log(anotherScore);
-
-
