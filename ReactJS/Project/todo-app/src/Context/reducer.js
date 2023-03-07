7efb4bba6d0a2c1da15f3feec3d9e4bb41b54c16
nikhilsomani15 <nikhilsomani15@gmail.com>
2023-03-07 15:58:03 +0530

    React todo app with local storage started

--- ReactJS/Project/todo-app/src/Context/reducer.js
+++ ReactJS/Project/todo-app/src/Context/reducer.js
@@ -2,7 +2,7 @@ import { ADD_TODO, REMOVE_TODO } from "./action.types";
 export default (state, action) => {
   switch (action.type) {
     case ADD_TODO:
-      // console.log(action.payload);
+      console.log(state);
       return [...state, action.payload];
     case REMOVE_TODO:
       return state.filter((todos) => todos.id !== action.payload);
