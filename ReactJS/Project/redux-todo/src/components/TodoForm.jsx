4434bf5d97c82a07a6b1d4f84c1504fd5f1e88ee
nikhilsomani15 <nikhilsomani15@gmail.com>
2023-03-18 11:24:58 +0530

    React redux todo app updated

---
+++ ReactJS/Project/redux-todo/src/components/TodoForm.jsx
@@ -0,0 +1,31 @@
+import { nanoid } from "@reduxjs/toolkit";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../features/todoSlice";
+
+import { useState } from "react";
+export const TodoForm = () => {
+  const [todoString, setTodoString] = useState("");
+  const dispatch = useDispatch();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (todoString == "") {
+      return alert("Please Enter a TODO");
+    }
+
+    dispatch(addTodo(todoString));
+
+    setTodoString("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={todoString}
+        onChange={(e) => setTodoString(e.target.value)}
+        placeholder="Enter Todo"
+      />
+      <button type="submit">Submit todo</button>
+    </form>
+  );
+};
--- ReactJS/Project/redux-todo/src/components/Todos.jsx
+++ ReactJS/Project/redux-todo/src/components/Todos.jsx
@@ -1,25 +1,23 @@
-import React from 'react'
-import {useSelector} from 'react-redux'
-import {useDispatch} from 'react-redux'
-import {removeTodo} from '../features/todoSlice'
+import React from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { removeTodo } from "../features/todoSlice";
+import { TodoForm } from "./TodoForm";
 
 function Todos() {
-    const todos = useSelector((state) => state.todos.todos)
-    const dispatch = useDispatch()
+  const todos = useSelector((state) => state.todos.todos);
+  const dispatch = useDispatch();
   return (
     <div>
-        {
-        todos.map((todo) => (
-          <>
-            <h1 key={todo.id}>{todo.text}</h1> 
-            <button
-            onClick={() => dispatch(removeTodo(todo.id))}
-            >X</button>
-            </>
-        ))
-        }
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <h1>{todo.text}</h1>
+          <button onClick={() => dispatch(removeTodo(todo.id))}>X</button>
         </div>
-  )
+      ))}
+      <TodoForm />
+    </div>
+  );
 }
 
-export default Todos
+export default Todos;
